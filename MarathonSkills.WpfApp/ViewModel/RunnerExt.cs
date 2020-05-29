8c39271a2ccc06090d0bd0dfe8bffab7a245d8e3
MRGRD56 <Bukinist56@mail.ru>
2020-05-29 16:13:44 +0500

    upd: RunnerSponsorPage ViewModel

--- MarathonSkills.WpfApp/ViewModel/RunnerExt.cs
+++ MarathonSkills.WpfApp/ViewModel/RunnerExt.cs
@@ -16,8 +16,9 @@ namespace MarathonSkills.WpfApp.ViewModel
 		/// <param name="ln">LastName</param>
 		/// <param name="cc">CountryCode</param>
 		/// <param name="bn">BibNumber</param>
-		public RunnerExt(int id, string fn, string ln, string cc, short? bn) => 
-			(RunnerId, FirstName, LastName, CountryCode, BibNumber) = (id, fn, ln, cc, bn);
+		/// <param name="ci">CharityId</param>
+		public RunnerExt(int id, string fn, string ln, string cc, short? bn, int ci) => 
+			(RunnerId, FirstName, LastName, CountryCode, BibNumber, CharityId) = (id, fn, ln, cc, bn, ci);
 
 		public int RunnerId { get; set; }
 
@@ -28,6 +29,8 @@ namespace MarathonSkills.WpfApp.ViewModel
 		public string CountryCode { get; set; }
 
 		public short? BibNumber { get; set; }
+
+		public int CharityId { get; set; }
 
 		public override string ToString() =>
 			$"{FirstName} {LastName} - {BibNumber} ({CountryCode})";
--- MarathonSkills.WpfApp/ViewModel/RunnerSporsorPageVM.cs
+++ MarathonSkills.WpfApp/ViewModel/RunnerSporsorPageVM.cs
@@ -1,4 +1,5 @@
-﻿using MarathonSkills.WpfApp.Extensions;
+﻿using MarathonSkills.WpfApp.DataModels;
+using MarathonSkills.WpfApp.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,6 +13,8 @@ namespace MarathonSkills.WpfApp.ViewModel
 	{
 		public ObservableCollection<RunnerExt> RunnersExts { get; set; }
 
+		public Charity SelectedCharity { get; set; }
+
 		public RunnerSporsorPageVM()
 		{
 			//TODO: лучше передавать ещё и Registration.RegistrationId
@@ -20,19 +23,27 @@ namespace MarathonSkills.WpfApp.ViewModel
 				r => r.Email, //Runners
 				u => u.Email, //Users
 				(r, u) => new { u.FirstName, u.LastName, r.CountryCode, r.RunnerId });
+
 			var ruRegistrations = runnersUsers
 				.Join(App.DbContext.Registrations.ToObsCol(),
 				ru => ru.RunnerId, //Runners & Users
 				g => g.RunnerId, //Registrations
-				(ru, g) => new { ru.FirstName, ru.LastName, ru.CountryCode, ru.RunnerId, g.RegistrationId });
+				(ru, g) => new { ru.FirstName, ru.LastName, ru.CountryCode, ru.RunnerId, g.RegistrationId, g.CharityId });
+
 			var rugEvents = ruRegistrations // Runners & Users & Registrations & RegistrationEvents
 				.Join(App.DbContext.RegistrationEvents.ToObsCol(),
 				rug => rug.RegistrationId, //Runners & Users & Regirstrations
 				e => e.RegistrationId, //RegistrationEvents
-				(rug, e) => new { rug.FirstName, rug.LastName, rug.CountryCode, e.BibNumber, rug.RunnerId });
+				(rug, e) => new { rug.FirstName, rug.LastName, rug.CountryCode, e.BibNumber, rug.RunnerId, rug.RegistrationId, rug.CharityId });
+
+			var rugeCharities = rugEvents // Runners & Users & Registrations & RegistrationEvents & Charities
+				.Join(App.DbContext.Charities.ToObsCol(),
+				ruge => ruge.CharityId,
+				c => c.CharityId,
+				(ruge, c) => new { ruge.FirstName, ruge.LastName, ruge.CountryCode, ruge.BibNumber, ruge.RunnerId, ruge.RegistrationId, c.CharityId });
 
 			RunnersExts = rugEvents
-				.Select(x => new RunnerExt(x.RunnerId, x.FirstName, x.LastName, x.CountryCode, x.BibNumber))
+				.Select(x => new RunnerExt(x.RunnerId, x.FirstName, x.LastName, x.CountryCode, x.BibNumber, x.CharityId))
 				.OrderBy(x => x.FirstName)
 				.ThenBy(x => x.LastName)
 				.ToObsCol();
